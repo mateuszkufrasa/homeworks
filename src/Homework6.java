b154319a2f488f4565ed8327c88f0b1d78268541
Mateusz Kufrasa <mateusz.kufrasa@gmail.com>
2020-01-14 23:03:55 +0100

    First commit

--- src/Homework6.java
+++ src/Homework6.java
@@ -5,17 +5,22 @@ public class Homework6 {
 
     public static void main(String[] args) throws IllegalArgumentException {
         Scanner input = new Scanner(System.in);
-	System.out.println("> Type a language mnemonic [EN, ES, FIN, FR, IT, GER, NOR, PL, UA]: ");
-	String choice = input.nextLine().toUpperCase();
-	try{
+	boolean correctData = false;
+
+	while (!correctData) {
+        correctData = true;
+        System.out.println("> Type a language mnemonic [EN, ES, FIN, FR, IT, GER, NOR, PL, UA]: ");
+        String choice = input.nextLine().toUpperCase();
+	try {
 	    Lang.valueOf(choice);
 	    System.out.println(choice);
+
     } catch (IllegalArgumentException e) {
-System.out.println("Try again: ");
-
-    } finally {
-
+    System.out.println("Try again: ");
+        correctData = false;
     }
+    break;
+	}
     }
 
     enum Lang {EN, ES, FIN, FR, IT, GER, NOR, PL, UA};
